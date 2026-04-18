f0d861a29eccff792e12168557bf363940b7aee7
EliseiSushko <153101829+EliseiSushko@users.noreply.github.com>
2026-04-18 18:20:36 +0500

    Update main.py
    Добавил движение  ракеток

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pygame import *
+
 from random import randint
 
 win_width = 700
@@ -11,30 +12,38 @@ window.fill((216, 219, 124))
 
 
 class GameSprite(sprite.Sprite):
-    def __init__(self,player_image,player_x,player_y, size_x, size_y):
+    def __init__(self,player_image,player_x,player_y,speed, size_x, size_y):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+        self.speed = speed
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
     
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-           self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-           self.rect.x += self.speed
+        if keys[K_UP] and self.rect.x > 5:
+           self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.x < win_width - 80:
+           self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.x > 5:
+           self.rect.y -= self.speed
+        if keys[K_s] and self.rect.x < win_width - 80:
+           self.rect.y += self.speed
 
 game = True
 finish = False
-ball = GameSprite('ball.png', 300, 200, 50 ,50)
-Rocket = Player('Rocket.png', 200, 300, 50, 100)
-Rocket1 = Player('Rocket.png', 150, 300, 50, 100)
+ball = GameSprite('ball.png', 325, 200, 4, 50 ,50)
+Rocket = Player('Rocket.png', 50, 200, 4, 50, 100)
+Rocket1 = Player('Rocket.png', 600, 200, 4, 50, 100)
 while game:
 
     
@@ -46,11 +55,13 @@ while game:
         
     if finish != True:
 
+        window.fill((216, 219, 124))
+
         ball.update()
         ball.reset()
-        Rocket.update()
+        Rocket.update_l()
         Rocket.reset()
-        Rocket1.update()
+        Rocket1.update_r()
         Rocket1.reset()
         
         display.update()
